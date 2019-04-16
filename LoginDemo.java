7637ae0c32745f3187ff5085b2cb6af091a51529
Mehedi-909 <46414380+Mehedi-909@users.noreply.github.com>
2019-04-16 09:51:48 +0600

    Add files via upload

---
+++ LoginDemo.java
@@ -0,0 +1,18 @@
+package lab;
+
+import javax.swing.JOptionPane;
+
+class LoginDemo
+{
+  public static void main(String arg[])
+  {
+  try
+  {
+  Login frame=new Login();
+  frame.setSize(300,100);
+  frame.setVisible(true);
+  }
+  catch(Exception e)
+  {JOptionPane.showMessageDialog(null, e.getMessage());}
+  }
+}
